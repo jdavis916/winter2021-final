1d36397bf49821b8b0a51e10be2e5ac5a96cbaea
CjL95 <71044991+CjL95@users.noreply.github.com>
2022-02-18 22:15:20 -0500

    foundation for stub

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,38 @@
+import {
+	dispName,
+	phone,
+	testimonials,
+	email,
+	about,
+	address
+} from '../stubs.js';
 var express = require('express');
 var router = express.Router();
+let title = 'Rocko Fitness';
 
 /* GET home page */
 router.get('/', function(req, res, next) {
   res.render('index', { 
-  	title: 'Simple Node Template',
-  	msg: 'This sample template should help get you on your way.',
+  	title: title,
+  	msg: 'Message',
   	pageMainClass: 'pgHome'
   });
+})
+
+//stub data debug, hit this route to see stubbed data
+.get('/stub', function(req, res, next) {
+	res.render('index', { 
+		title: title,
+		msg: 'Message',
+		pageMainClass: 'pgHome',
+		about: about,
+		testimonials: testimonials,
+		dispName: dispName,
+		email: email,
+		phone: phone,
+		address: address
+
+	});
 });
 
 module.exports = router;
