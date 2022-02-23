638077a78eb6dd8f8013e1eb6703af77804c1447
yaredhadgu <96091815+yaredhadgu@users.noreply.github.com>
2022-02-23 14:05:27 -0800

    stub address data

--- stubs.js
+++ stubs.js
@@ -4,10 +4,10 @@ let testimonials = ''; //string
 let email = ''; //string
 let about = ''; //string
 let address = {
-    'street': '',
-    'city': '',
-    'state': '',
-    'zip': 0
+    'street': '123 Lorem Dr',
+    'city': 'San jose',
+    'state':'CA ',
+    'zip':95117
 };
 
 module.exports = {
