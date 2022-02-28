1a97e27cdfbbaa2aafbe1699b365c902430e5fb1
CjL95 <71044991+CjL95@users.noreply.github.com>
2022-02-27 21:24:50 -0500

    added password encryption

--- controllers/strategy.js
+++ controllers/strategy.js
@@ -5,28 +5,34 @@ const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 var db = mongoose.connection;
 //var signInError = false;
-//stores a token for the session in the browser cookie
-passport.serializeUser((user, done)=>{
+
+//responsible for storing reference to user in the cookie
+passport.serializeUser((user, done)=>{        //stores a token for the session in the browser cookie
 	done(null, user.id);
 });
-//takes a token from the cookie and converts it to user id
-passport.deserializeUser((id, done)=>{
+
+passport.deserializeUser((id, done)=>{       //takes a token from the cookie and converts it to user id
 	userModel.findById(id, (err, user)=> {
 		done(err, user);
 	});
 });
+
+
 //strategy for passport to handle sign in validation
 passport.use(
 	new LocalStrategy(
   	function(req, username, pw, done) {
+        console.log('user : ' + user);
 //finds a user by the info entered in. returns an error if user isn't found
   	  userModel.findOne({ username: username }, function (err, user) {
+        
   	    if (err) { return done(err);}
   	    if (!user) { return done(null, false);}
   	    if (!user.verifyPassword(pw)) { return done(null, false); }
   	    return done(null, user);
   	  });
   	}
-	));
+    )
+);
 
 module.exports = passport;
